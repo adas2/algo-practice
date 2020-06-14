17e3761f5a8cdfb1bff4208b4cafa84efd1bce45
Abhishek Das <a.das@salesforce.com>
2020-06-13 23:26:34 -0400

    fixup

--- gopractice/sorted_median.go
+++ gopractice/sorted_median.go
@@ -3,7 +3,7 @@ package practice
 import "fmt"
 
 // Find median of two sorted arrays (of uequal sizes)
-// for equal size array median, simpler algo exits
+// for equal size array median, simpler algo exits (hint: merge sort merge op)
 // complexity: O(log(min(n, m)))
 
 func findSortedMedian(arr1, arr2 []int) float32 {
@@ -51,33 +51,44 @@ func findSortedMedian(arr1, arr2 []int) float32 {
 			// move left in arr1, reduced search space
 			maxIdx = i - 1
 		} else {
-			// desired cond is reached, i.e. arr1[i-1] < arr2[j] && arr1[j-1] > arr2[i]
+			// desired cond is reached,
+			// i.e. arr1[i-1] < arr2[j] && arr1[j-1] > arr2[i]
 
 			// two cases even and odd
-			if even {
-				if i == 0 {
-					median = float32((arr2[j-1] + arr2[j]) / 2)
-				} else if j == 0 {
-					median = float32((arr1[i-1] + arr1[i])) / 2
-				} else {
-					median = float32((max(arr1[i-1], arr2[j-1]) + min(arr1[i], arr2[j])) / 2)
-				}
+			// within each cosider when:
+			// left half of arr1 is nil,
+			// left half of arr2 is nil
+			if i == 0 {
+				median = float32(arr2[j-1])
+			} else if j == 0 {
+				median = float32(arr1[i-1])
 			} else {
-				if i == 0 {
-					median = float32(arr2[j-1])
-				} else if j == 0 {
-					median = float32(arr1[i-1])
-				} else {
-					median = float32(max(arr1[i-1], arr2[j-1]))
-				}
+				median = float32(max(arr1[i-1], arr2[j-1]))
 			}
 			break
+		}
+	}
+
+	// evaluate other half in necessary (even case)
+	// i.e. min of the elements in right half
+	if even {
+		// consider
+		// right half of arr1 is nil
+		// right half of arr2 is nil
+		if i == len(arr1) {
+			median = (median + float32(arr2[j])) / 2
+		} else if j == len(arr2) {
+			median = (median + float32(arr1[i])) / 2
+		} else {
+			// normal case
+			median = (median + float32(min(arr1[i], arr2[j]))) / 2
 		}
 	}
 
 	return median
 }
 
+// util funcs
 func max(a, b int) int {
 	if a < b {
 		return b
