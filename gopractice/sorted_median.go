22535f2b24c6dfcbaf289932193518b55574283d
adas2 <ada829@gmail.com>
2023-01-26 16:21:16 -0500

    Separated utils.go

--- gopractice/sorted_median.go
+++ gopractice/sorted_median.go
@@ -87,18 +87,3 @@ func findSortedMedian(arr1, arr2 []int) float32 {
 
 	return median
 }
-
-// util funcs
-func max(a, b int) int {
-	if a < b {
-		return b
-	}
-	return a
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
---
+++ gopractice/utils.go
@@ -0,0 +1,17 @@
+// Utility funcs used by other modules
+
+package practice
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
