a194749553ab49db8331d75ac6cdcbcb6441c821
adas2 <ada829@gmail.com>
2023-01-27 01:18:56 -0500

    test rsa cert gen

---
+++ gopractice/crypto/cert_gen.go
@@ -0,0 +1,40 @@
+// Test code generated from ChatGPT to generate RSA cert + private key
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"time"
+)
+
+func main() {
+	// Generate a new RSA key
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	// Create a template for the certificate
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			CommonName: "example.com",
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().Add(time.Hour * 24 * 365), // valid for 1 year
+		KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+
+	// Create the certificate
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
+
+	// Encode the certificate and private key in PEM format
+	certOut := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	keyOut := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)})
+
+	// Print the PEM-encoded certificate and private key
+	fmt.Printf("%s\n%s", certOut, keyOut)
+}
